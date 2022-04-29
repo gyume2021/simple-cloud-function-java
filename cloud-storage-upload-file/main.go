14b08dd08593a3d8187ebdb2d033c2fd7cb90872
gyume2021 <letsencrypthuang@gmail.com>
2022-04-29 10:02:13 +0800

    add cloud storage upload file example

---
+++ cloud-storage-upload-file/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+var (
+	ProjectID  string
+	bucketName string
+	file       string
+)
+
+func main() {
+
+	// flag.IntVar(&port, "port", 9125, "Port to listen on")
+	// flag.IntVar(&port, "p", 9125, "Port to listen on")
+
+	flag.StringVar(&ProjectID, "p", "", "Project ID")
+	flag.StringVar(&bucketName, "b", "", "bucket name")
+	flag.StringVar(&file, "f", "", "file path")
+
+	flag.Parse()
+
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// upload file
+	if err := UploadFile(ioutil.Discard, bucketName, file); err != nil {
+		log.Fatalf("uploadFile(%q): %v", file, err)
+	}
+
+}
+
+// uploadFile uploads an object.
+func UploadFile(w io.Writer, bucket, object string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	// Open local file.
+	f, err := os.Open(object)
+	if err != nil {
+		return fmt.Errorf("os.Open: %v", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	o := client.Bucket(bucket).Object(object)
+
+	// Optional: set a generation-match precondition to avoid potential race
+	// conditions and data corruptions. The request to upload is aborted if the
+	// object's generation number does not match your precondition.
+	// For an object that does not yet exist, set the DoesNotExist precondition.
+	o = o.If(storage.Conditions{DoesNotExist: true})
+	// If the live object already exists in your bucket, set instead a
+	// generation-match precondition using the live object's generation number.
+	// attrs, err := o.Attrs(ctx)
+	// if err != nil {
+	// 	return fmt.Errorf("object.Attrs: %v", err)
+	// }
+	// o = o.If(storage.Conditions{GenerationMatch: attrs.Generation})
+
+	// Upload an object with storage.Writer.
+	wc := o.NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+	fmt.Fprintf(w, "Blob %v uploaded.\n", object)
+	return nil
+}
